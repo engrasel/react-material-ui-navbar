9b29e2858d81557b90757a47690532f679c6b063
engrasel <contact.engrasel@gmail.com>
2021-12-28 19:12:28 +0600

    first commit

---
+++ src/components/ResponsiveAppBar/TemporaryDrawer.js
@@ -0,0 +1,71 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { NavbarData } from "./NavbarData";
+
+export default function SwipeableTemporaryDrawer() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => event => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = anchor => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {NavbarData.map((item, index) => (
+          <ListItem button key={index}>
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText>{item.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      {["left", "right", "top", "bottom"].map(anchor => (
+        <React.Fragment key={anchor}>
+          <Button
+            onClick={toggleDrawer(anchor, true)}
+            style={{ color: "white" }}
+          >
+            {anchor}
+          </Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
